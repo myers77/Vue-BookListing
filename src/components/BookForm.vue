28a7750709046dc5961a011f101cef48b6a741ec
Myers <bmyers@uscvgnb45823.insight.com>
2019-04-29 15:03:26 -0400

    Add BookForm

---
+++ src/components/BookForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <form action="#" method="post" v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)">
+    <input type="text" name="title" v-model="bookTitle" placeholder="Book Title">
+    <input type="text" name="author" v-model="bookAuthor" placeholder="Book Author">
+    <button type="submit" name="button">Add book</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "BookForm",
+  props: ["books"],
+  data() {
+    return {
+      bookTitle: "",
+      bookAuthor: ""
+    };
+  },
+  methods: {
+    bookSubmit(bookTitle, bookAuthor) {
+      this.$emit("addBook", bookTitle, bookAuthor);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,16 +4,27 @@
     <ul>
       <book-item v-for="book in books" :key="book.title" :book="book"></book-item>
     </ul>
+    <book-form @addBook="appendBook"></book-form>
   </div>
 </template>
 
 <script>
 import BookItem from "./BookItem";
+import BookForm from "./BookForm";
 
 export default {
   name: "Booklist",
   components: {
-    BookItem
+    BookItem,
+    BookForm
+  },
+  methods: {
+    appendBook(bookTitle, bookAuthor) {
+      this.books.push({
+        title: bookTitle,
+        author: bookAuthor
+      });
+    }
   },
   data() {
     return {
