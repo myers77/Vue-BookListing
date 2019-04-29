a2fcad0548ed9a064c7a160f0f3e1d5299218d36
Myers <bmyers@uscvgnb45823.insight.com>
2019-04-29 13:55:12 -0400

    adding BookList

---
+++ src/components/BookList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Booklist",
+  data() {
+    return { title: "All Books" };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+h1,
+h2 {
+  font-weight: "normal";
+}
+</style>
